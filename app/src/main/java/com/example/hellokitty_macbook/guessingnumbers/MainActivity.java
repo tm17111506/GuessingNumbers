c122ef631a98c7bc2b49c7a712f869404bf30d6e
tm17111506 <brownie.tm@gmail.com>
2015-09-03 20:03:13 +0800

    Implement MainActivity

--- app/src/main/java/com/example/hellokitty_macbook/guessingnumbers/MainActivity.java
+++ app/src/main/java/com/example/hellokitty_macbook/guessingnumbers/MainActivity.java
@@ -4,13 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
+
+    private Random rand;
+    private Integer target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.rand = new Random();
+        this.target = this.rand.nextInt(1000);
     }
 
     @Override
@@ -33,5 +44,29 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onGoButtonClicked(View view) {
+        EditText numberField = (EditText) findViewById(R.id.editText);
+        TextView tv = (TextView) findViewById(R.id.message);
+
+        String currentNumberStr = numberField.getText().toString();
+        numberField.setText("");
+
+        if (currentNumberStr.matches("\\d+")) {
+            Integer currentNumber = Integer.parseInt(currentNumberStr);
+            String msg;
+            if (currentNumber > this.target) {
+                msg = String.format("Smaller than %s", currentNumber);
+            } else if (currentNumber < this.target) {
+                msg = String.format("Larger than %s", currentNumber);
+            } else {
+                msg = "BOOM!";
+                this.target = this.rand.nextInt(1000);
+            }
+            tv.setText(msg);
+        } else {
+            tv.setText("Not a number!");
+        }
     }
 }
